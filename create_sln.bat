909d089633a3c04b8d86ae088c6d15703fc0b0c1
craftablescience <me@craftablescience.info>
2025-03-11 08:57:25 -0400

    fix: use correct platform in sln batch script

--- create_sln.bat
+++ create_sln.bat
@@ -3,7 +3,7 @@ cd /d %~dp0
 
 echo Running CMake...
 echo/
-cmake -B build -G "Visual Studio 17 2022"
+cmake -B build -G "Visual Studio 17 2022" -A "Win32"
 echo/
 echo Assuming CMake setup went well, the project sln is located at "build/xzptool.sln".
 echo/
